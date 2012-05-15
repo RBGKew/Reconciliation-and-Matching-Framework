d38eb08d6662ae0cd176f7da0b65d63bc48e1b33
Nicky Nicolson <n.nicolson@kew.org>
2012-05-15 10:13:10 +0100

    Added config for the top copy report

--- src/main/java/org/kew/shs/dedupl/configuration/DeduplicationConfiguration.java
+++ src/main/java/org/kew/shs/dedupl/configuration/DeduplicationConfiguration.java
@@ -5,13 +5,39 @@ import java.io.File;
 public class DeduplicationConfiguration extends Configuration {
 
 	private File dataFile;
-
+	private File topCopyFile;
+	private boolean writeTopCopyReport=false;
+	private String scoreFieldName;
+	
 	public File getDataFile() {
 		return dataFile;
 	}
 
 	public void setDataFile(File dataFile) {
 		this.dataFile = dataFile;
+	}
+
+	public File getTopCopyFile() {
+		return topCopyFile;
+	}
+
+	public void setTopCopyFile(File topCopyFile) {
+		this.topCopyFile = topCopyFile;
+	}
+
+	public boolean isWriteTopCopyReport() {
+		return writeTopCopyReport;
+	}
+	public void setWriteTopCopyReport(boolean writeTopCopyReport) {
+		this.writeTopCopyReport = writeTopCopyReport;
+	}
+
+	public String getScoreFieldName() {
+		return scoreFieldName;
+	}
+
+	public void setScoreFieldName(String scoreFieldName) {
+		this.scoreFieldName = scoreFieldName;
 	}
 	
 }
