95a192b98556af75b1d967f051bb58f506b20699
Alecs Geuder <a.geuder@kew.org>
2013-02-13 16:56:54 +0000

    IntegerMatcher
    * alternative as for example a year matcher to initial substringmatcher,
      accepts a maxDiff +-
    * NOTE: a similar functionality is integrated into lucene's core (at
      least of v.4+), should be replaced by an implementation of that?

---
+++ src/main/java/org/kew/shs/dedupl/matchers/IntegerMatcher.java
@@ -0,0 +1,29 @@
+package org.kew.shs.dedupl.matchers;
+
+public class IntegerMatcher implements Matcher {
+
+	public static int COST = 1;
+	private int maxDiff = 5;
+
+	@Override
+	public boolean matches(String s1, String s2) {
+		return Math.abs((Integer.parseInt(s1) - Integer.parseInt(s2))) <= this.maxDiff;
+	}
+
+	@Override
+	public boolean isExact() {
+		return false;
+	}
+
+	@Override
+	public int getCost() {
+		return COST;
+	}
+
+	@Override
+	public String getExecutionReport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
