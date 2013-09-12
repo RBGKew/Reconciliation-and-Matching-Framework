346c46700aa6f1ad46604839399df939a35a93bc
Alecs Geuder <a.geuder@kew.org>
2013-09-13 00:20:21 +0100

    CommonTokensMatcher: uniqueCommonTokens
    * added a flag 'uniqueCommonTokens' as behaviour seems weird in case
      of multiple occurring common tokens (see test) -- that flag garants
      uniqueness of common tokens

--- deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/matchers/CommonTokensMatcher.java
+++ deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/matchers/CommonTokensMatcher.java
@@ -3,6 +3,7 @@ package org.kew.shs.dedupl.matchers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.kew.shs.dedupl.util.LibraryRegister;
 import org.slf4j.Logger;
@@ -18,70 +19,80 @@ import org.slf4j.LoggerFactory;
 @LibraryRegister(category="matchers")
 public class CommonTokensMatcher implements Matcher {
 
-	public static int COST = 5;
-	protected double minRatio = 0.5;
-	private String delimiter = " ";
-	
+    public static int COST = 5;
+    protected double minRatio = 0.5;
+    private String delimiter = " ";
+    private boolean uniqueCommonTokens = false;
+
     private static Logger logger = LoggerFactory.getLogger(CommonTokensMatcher.class);
-	
-	public int getCost() {
-		return COST;
-	}
-	
-	/*@Cacheable(cacheName="ctMatchCache")*/
-	public boolean matches(String s1, String s2) throws Exception {
-		logger.debug("s1: " + s1);
-		logger.debug("s2: " + s2);
-		if (s1 == null && s2 == null) return true;
-	    String[] a1 = convToArray(s1);
-	    logger.debug(Arrays.toString(a1));
-	    String[] a2 = convToArray(s2);
-	    logger.debug(Arrays.toString(a2));
-		return calculateTokensInCommon(a1,a2);
-	}
 
-	private String[] convToArray(String s){
-		String[] a = s.split(this.delimiter);
-		// if delimiter is blank we want every element to be represented as one item in the array;
-		// however, the first element would be blank, which we correct here.
-		if (this.getDelimiter() == "") a = Arrays.copyOfRange(a, 1, a.length);
-		return a;
-	}
-	
-	public Boolean calculateTokensInCommon(String[] s1, String[] s2){
-		Collection<String> common = new ArrayList<String>(Arrays.asList(s1));
-	    common.retainAll(Arrays.asList(s2));
-	    int numCommon = common.size();
-	    logger.debug("Number of tokens in common: " + numCommon);
-	    
-	    double ratio = (Double.valueOf(numCommon) / Double.valueOf((numCommon + (s1.length - numCommon) + (s2.length - numCommon))));
-	    logger.debug("ratio = " + ratio);
-	    
-	    return ratio >= this.minRatio;
-	}
-	
-	public double getMinRatio() {
-		return this.minRatio;
-	}
+    public int getCost() {
+        return COST;
+    }
 
-	public void setMinRatio(double minRatio) {
-		this.minRatio = minRatio;
-	}
-	
-	public boolean isExact() {
-		return false;
-	}
-	
-	public String getExecutionReport() {
-		return null;
-	}
+    /*@Cacheable(cacheName="ctMatchCache")*/
+    public boolean matches(String s1, String s2) throws Exception {
+        logger.debug("s1: " + s1);
+        logger.debug("s2: " + s2);
+        if (s1 == null && s2 == null) return true;
+        String[] a1 = convToArray(s1);
+        logger.debug(Arrays.toString(a1));
+        String[] a2 = convToArray(s2);
+        logger.debug(Arrays.toString(a2));
+        return calculateTokensInCommon(a1,a2);
+    }
 
-	public String getDelimiter() {
-		return delimiter;
-	}
+    private String[] convToArray(String s){
+        String[] a = s.split(this.delimiter);
+        // if delimiter is blank we want every element to be represented as one item in the array;
+        // however, the first element would be blank, which we correct here.
+        if (this.getDelimiter() == "") a = Arrays.copyOfRange(a, 1, a.length);
+        return a;
+    }
 
-	public void setDelimiter(String delimiter) {
-		this.delimiter = delimiter;
-	}
-	
+    public Boolean calculateTokensInCommon(String[] s1, String[] s2){
+        Collection<String> common = new ArrayList<String>(Arrays.asList(s1));
+        common.retainAll(Arrays.asList(s2));
+        if (this.isUniqueCommonTokens()) common = new HashSet<String>(common);
+        int numCommon = common.size();
+        logger.debug("Number of tokens in common: " + numCommon);
+
+        double ratio = (Double.valueOf(numCommon) / Double.valueOf((numCommon + (s1.length - numCommon) + (s2.length - numCommon))));
+        logger.debug("ratio = " + ratio);
+
+        return ratio >= this.minRatio;
+    }
+
+    public double getMinRatio() {
+        return this.minRatio;
+    }
+
+    public void setMinRatio(double minRatio) {
+        this.minRatio = minRatio;
+    }
+
+    public boolean isExact() {
+        return false;
+    }
+
+    public String getExecutionReport() {
+        return null;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public boolean isUniqueCommonTokens() {
+        return uniqueCommonTokens;
+    }
+
+    public void setUniqueCommonTokens(boolean uniqueCommonTokens) {
+        this.uniqueCommonTokens = uniqueCommonTokens;
+    }
+
 }
