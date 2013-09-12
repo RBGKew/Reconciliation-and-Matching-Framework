d3f548f9477dc5962efca1bf7da1cd2bf58269e5
Alecs Geuder <a.geuder@kew.org>
2013-09-12 16:34:09 +0100

    EX,ex,eX,Ex,IN,in,In,iN

--- deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/RegexDefCollection.java
+++ deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/RegexDefCollection.java
@@ -4,9 +4,9 @@ package org.kew.shs.dedupl.transformers;
 public abstract class RegexDefCollection {
 
 	protected String EX_MARKER = " ex ";
-	protected String EX_MARKER_REGEX = "( ex | Ex )";
+	protected String EX_MARKER_REGEX = "( [Ee][Xx] )";
 	protected String IN_MARKER = " in ";
-	protected String IN_MARKER_REGEX = "( in | In )";
+	protected String IN_MARKER_REGEX =  "( [Ii][Nn] )";
 
 	// to catch (more than one) occurrences of bracket-pairs incl. their content
 	protected String ROUND_BRACKETS_AND_CONTENT = "\\([^\\(\\)]*?\\)";
--- deduplicator-framework-app/src/test/java/org/kew/shs/dedupl/transformers/authors/StripExAuthorTransformerTest.java
+++ deduplicator-framework-app/src/test/java/org/kew/shs/dedupl/transformers/authors/StripExAuthorTransformerTest.java
@@ -19,6 +19,12 @@ public class StripExAuthorTransformerTest {
         Transformer transformer = new StripExAuthorTransformer();
         String author = "Someone ex Else";
         assertEquals("Else", transformer.transform(author));
+        author = "Someone Ex Else";
+        assertEquals("Else", transformer.transform(author));
+        author = "Someone eX Else";
+        assertEquals("Else", transformer.transform(author));
+        author = "Someone EX Else";
+        assertEquals("Else", transformer.transform(author));
     }
 
 }
