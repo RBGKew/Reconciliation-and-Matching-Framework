90ad97551fd8f2ab62ccdde91a4fa891774f6adc
Alecs Geuder <a.geuder@kew.org>
2013-09-12 15:58:41 +0100

    updated doc of RemoveBracketedTextTransformer

--- deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/RemoveBracketedTextTransformer.java
+++ deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/RemoveBracketedTextTransformer.java
@@ -3,7 +3,7 @@ package org.kew.shs.dedupl.transformers;
 import org.kew.shs.dedupl.util.LibraryRegister;
 
 /*
- * Removes all text in brackets (round and square) incl. the brackets.
+ * Removes all text in brackets (round, square and curly) incl. the brackets.
  *
  * TODO: not sure how that deals with nested structures, needs more testing
  */
