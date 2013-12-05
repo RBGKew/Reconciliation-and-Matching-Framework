6488df3c7b3cff4fdb93ed3f511bbb155c45368e
Alecs Geuder <a.geuder@kew.org>
2013-12-05 20:58:28 +0000

    Added CollationTransformers to MatchConf
    * annotated the new CollationTransformers to be picked up by MatchConf

--- deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/collations/VolExtractorTransformer.java
+++ deduplicator-framework-app/src/main/java/org/kew/shs/dedupl/transformers/collations/VolExtractorTransformer.java
@@ -1,7 +1,9 @@
 package org.kew.shs.dedupl.transformers.collations;
 
 import org.kew.shs.dedupl.transformers.Transformer;
+import org.kew.shs.dedupl.util.LibraryRegister;
 
+@LibraryRegister(category="transformers")
 public class VolExtractorTransformer implements Transformer{
 
 	@Override
